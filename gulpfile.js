50fd1add612bc340c4f665934a828428b9904075
ministori <ministori@gmail.com>
2018-09-17 10:03:04 +0900

    메이크올 작업

--- gulpfile.js
+++ gulpfile.js
@@ -6,59 +6,153 @@ var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var concat = require('gulp-concat');
 var webserver = require('gulp-webserver');
+var fileData = require('gulp-pub-list');
+var minify = require('gulp-minify');
 
-gulp.task('check', function() {
-    return gulp.src(['*', 'js/*', 'css/*', 'html/*'])
-        .pipe(livereload());
-});
-gulp.task('watch', function() {
-    livereload.listen();
-    gulp.watch('*', ['check']);
-    gulp.watch('js_src/**', ['concat', 'copy', 'check']);
-    gulp.watch('css_scss/**', ['sass', 'check']);
-    gulp.watch('html_src/**', ['include', 'check']);
-});
+/**
+ * build default
+ */
 
-gulp.task('include', function(){
-
-    var main = gulp.src(['html_src/*.html'])
-        .pipe(include())
-        .on('error', console.log)
-        .pipe(gulp.dest('html/'));
-
-    var sub = gulp.src(['html_src/sub/*.html'])
-        .pipe(include())
-        .on('error', console.log)
-        .pipe(gulp.dest('html/sub/'));
-
+// build html including header/footer
+gulp.task('build:include:html', function(){
+  gulp.src(['_dev_src/html/*.html'])
+      .pipe(include())
+      .on('error', console.log)
+      .pipe(gulp.dest('html/'));
 });
 
-gulp.task('sass', function(){
-    return gulp.src('css_scss/*.scss')
-        .pipe(sourcemaps.init())
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(sourcemaps.write())
-        .pipe(gulp.dest('css/'));
+// build sass for dev
+gulp.task('build:sass:dev', function(){
+  gulp.src('_dev_src/scss/*.scss')
+      .pipe(sourcemaps.init())
+      .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest('css/'));
 });
 
-gulp.task('concat', function() {
-    return gulp.src('js_src/*.js')
-        .pipe(concat('function.js'))
-        .pipe(gulp.dest('js/'));
+// build js compress
+gulp.task('build:js:compress', function(){
+  gulp.src(['_dev_src/js/*.js'])
+      .pipe(concat('function.js'))
+      .pipe(minify({
+        ext:{
+          src : '.debug.js',
+          min : '.min.js'
+        }
+      }))
+      .pipe(gulp.dest('js/'));
 });
 
-gulp.task('copy', function () {
-    return gulp.src('js_src/lib/**')
-        .pipe(gulp.dest('js/lib/'));
+/**
+ * build seperately
+ */
+
+// copy js library file
+gulp.task('seperate:copy:jsLib', function() {
+  gulp.src('_dev_src/js/lib/*.*')
+      .pipe(gulp.dest('js/lib/'));
 });
 
+// build file list to json data
+gulp.task('seperate:build:fileListJson', function() {
+  fileData('./html/', './_dev_src/data/file_list.json');
+});
+
+// copy file list json
+// gulp.task('seperate:copy:fileListJson', function(){
+//   gulp.src('_dev_src/data/*.json')
+//       .pipe(concat('file_data.json'))
+//       .pipe(gulp.dest('_dev_src/data/'));
+// });
+
+/**
+ * release
+ */
+
+// release html
+gulp.task('release:html', function(){
+  gulp.src(['_dev_src/html/*.html'])
+      .pipe(include())
+      .on('error', console.log)
+      .pipe(gulp.dest('release/html/'));
+});
+
+// release sass
+gulp.task('release:sass', function(){
+  gulp.src('_dev_src/scss/*.scss')
+      .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+      .pipe(gulp.dest('release/css/'));
+});
+
+// release js
+gulp.task('release:js:compress', function(){
+  gulp.src(['_dev_src/js/*.js'])
+      .pipe(concat('function.js'))
+      .pipe(minify({
+        ext:{
+          src : '.debug.js',
+          min : '.min.js'
+        }
+      }))
+      .pipe(gulp.dest('release/js/'));
+});
+
+// release js library file
+gulp.task('release:copy:jsLib', function() {
+  return gulp.src('_dev_src/js/lib/*.*')
+      .pipe(gulp.dest('release/js/lib/'));
+});
+
+// release images
+gulp.task('release:images', function(){
+  return gulp.src('images/*.*')
+      .pipe(gulp.dest('release/images/'));
+});
+
+// release fonts
+gulp.task('release:fonts', function(){
+  return gulp.src('fonts/**')
+      .pipe(gulp.dest('release/fonts/'));
+});
+
+/**
+ * reload
+ */
+
+// live reload
+gulp.task('reload:livereload', function() {
+  gulp.src(['*', 'html/*', 'css/*', 'js/*'])
+      .pipe( livereload() );
+});
+
+// watch
+gulp.task('reload:watch', function() {
+  livereload.listen();
+  gulp.watch('*', ['reload:livereload']);
+  gulp.watch('_dev_src/html/**', ['build:include:html', 'reload:livereload']);
+  gulp.watch('_dev_src/scss/**', ['build:sass:dev', 'reload:livereload']);
+  gulp.watch('_dev_src/js/**', ['build:js:compress', 'reload:livereload']);
+});
+
+// run project webserver
 gulp.task('webserver', function(){
-    gulp.src('./')
-        .pipe(webserver({
-            livereload: true,
-            open: true,
-            port:8888
-        }))
+  gulp.src('./')
+      .pipe(webserver({
+        livereload: true,
+        open: true,
+        port:8888
+      }));
 });
 
-gulp.task('default', ['check', 'watch', 'include', 'sass', 'concat', 'copy', 'webserver']);
+/**
+ * run task
+ */
+
+// build task
+gulp.task('default', ['build:include:html', 'build:sass:dev', 'build:js:compress', 'reload:watch', 'webserver']);
+
+// seperate task
+gulp.task('seperate', ['seperate:copy:jsLib', 'seperate:build:fileListJson']);
+
+// release task
+gulp.task('release', ['release:html', 'release:sass', 'release:js:compress', 'release:copy:jsLib', 'release:images', 'release:fonts']);
